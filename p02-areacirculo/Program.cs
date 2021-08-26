b3da53054645cfe910880ac23b1ef61c4718b47c
Eugenia18 <ecm.1996.uaz@gmail.com>
2021-08-25 22:59:14 -0600

    va de nuevo

--- p02-areacirculo/Program.cs
+++ p02-areacirculo/Program.cs
@@ -13,7 +13,7 @@ namespace p02_areacirculo
             Console.WriteLine("Calculando el Area de un Circulo");
             Console.WriteLine("Dame el radio?");
             radio = float.Parse(Console.ReadLine());
-            area = Math.PI * Math.Pow(radio,2); //Elevar al cuadrado
+            area = Math.PI * Math.Pow(radio,2);
             Console.WriteLine($"El area del circulo es {area}");
 
         }
